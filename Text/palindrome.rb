4f092592f2ddf3b0a5644148dc7175a9c81e86fa
petehuang <peteh22@gmail.com>
2013-10-03 22:01:26 -0500

    palindrome

---
+++ Text/palindrome.rb
@@ -0,0 +1,20 @@
+def palindrome?(string)
+	#in ruby, we can just see if string.reverse == string. but let's try harder than that, shall we?
+	f = 0
+	l = string.size - 1
+	until l <= f
+		return false if string[f] != string[l]
+		f += 1
+		l -= 1
+	end
+	return true
+end
+
+puts "Enter a string: "
+string = gets.strip.downcase
+if palindrome?(string)
+	puts "#{string} is a palindrome."
+else
+	puts "#{string} is not a palindrome."
+end
+
