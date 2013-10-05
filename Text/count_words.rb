092c03e12a89877ff59a9c7dbf498c4baa7f1dae
petehuang <peteh22@gmail.com>
2013-10-04 21:53:58 -0500

    word count

---
+++ Text/count_words.rb
@@ -0,0 +1,18 @@
+def word_count(string)
+	string.gsub!(/\W/, ' ')
+	string.split(" ").length
+end
+
+#String input
+
+#puts "Enter a string: "
+#string = gets.strip
+#puts "There are #{word_count(string)} words in the string"
+
+puts "Where is the file? "
+filepath = gets.strip
+totalwords = 0
+IO.foreach(filepath) do |line|
+	totalwords += word_count(line)
+end
+puts "There are #{totalwords} words in the file"
